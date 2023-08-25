12b73ec09d6debc3878081f2e3a8342c089b126e
Rick96b <olegkorobeinikov21@gmail.com>
2023-08-25 20:44:25 +0500

    fsd template

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { withProviders } from "./providers";
+
+import { Routing } from 'pages';
+import './styles';
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routing />
   );
 }
 
-export default App;
+export default withProviders(App);
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { lazy } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+//const TestPage = lazy(() => import("./test"));
+
+export const Routing = () => {
+    return (
+        <Routes>
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+};
